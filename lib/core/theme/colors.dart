a2b4be49483ce13477086cb44dbe90186519e3d5
Mohamed Atta <mohamedatta24@gmail.com>
2026-06-08 09:01:47 +0300

    finsh on boarding ui

--- lib/core/theme/colors.dart
+++ lib/core/theme/colors.dart
@@ -5,4 +5,5 @@ class AppColors {
   static const Color grey = Color(0xFF757575);
   static const Color white = Color(0xFFFFFFFF);
   static const Color black = Color(0xFF000000);
+  static const Color lightGrey = Color(0xFFF5F5F5);
 }
